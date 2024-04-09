7910fb99219b237f333c549efd54ddb1002a3c1a
Esnobart <offrichterrr@gmail.com>
2024-04-09 21:29:17 +0100

    first start

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,33 @@
-import contactsService from "../services/contactsServices.js";
+import { addContact, getContactById, listContacts, removeContact, updContact } from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+    const getContacts = await listContacts();
+    res.send(getContacts);
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+    const getContact = await getContactById(req.params.id);
+    if (!getContact) {res.status(404).json({message: "Contact is not found"})}
+    res.status(200).send(getContact)
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+    const delContact = await removeContact(req.params.id);
+    if (!delContact) {res.status(404).json({message: "Contact is not found"})}
+    res.status(200).send(delContact)
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res) => {
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {res.status(400).json({ message: "Body must have at least one field"})};
+    const newContact = await addContact(name, email, phone);
+    if (!newContact) {res.status(404).json({message: "Contact is already exist"})};
+    res.status(201).send(newContact)
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {res.status(400).json({ message: "Body must have at least one field"})};
+    const upContact = await updContact(req.params.id, name, email, phone);
+    res.status(200).send(upContact)
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,75 @@
+import fs from 'fs';
+import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+
+const contactsPath = path.join('db', 'contacts.json');
+
+async function listContacts() {
+    try {
+      const data = await fs.promises.readFile(contactsPath);
+      return JSON.parse(data);
+    } catch (error) {
+      return [];
+    }
+}
+
+async function getContactById(contactId) {
+    try {
+      const data = await fs.promises.readFile(contactsPath);
+      const contacts = JSON.parse(data);
+      return contacts.find(contact => contact.id === contactId) || null;
+    } catch (error) {
+      return null;
+    }
+}
+
+async function removeContact(contactId) {
+    try {
+        const data = await fs.promises.readFile(contactsPath);
+        const contacts = JSON.parse(data);
+        const removedContact = contacts.find(contact => contact.id === contactId);
+        if (!removedContact) return null;
+        const updatedContacts = contacts.filter(contact => contact.id !== contactId);
+        await fs.promises.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+        return removedContact;
+    } catch (error) {
+      return null;
+    }
+}
+  
+async function addContact(name, email, phone) {
+    try {
+        const data = await fs.promises.readFile(contactsPath);
+        const contacts = JSON.parse(data);
+        const nameExist = contacts.find(contact => contact.name === name);
+        const emailExist = contacts.find(contact => contact.email === email);
+        const phoneExist = contacts.find(contact => contact.phone === phone);
+        if (!nameExist && !emailExist && !phoneExist) {
+            const newContact = { id: uuidv4(), name, email, phone };
+            contacts.push(newContact);
+            await fs.promises.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+            return newContact;
+        }
+        return null;
+    } catch (error) {
+      return null;
+    }
+}
+
+async function updContact(id, name, email, phone) {
+    try {
+        const data = await fs.promises.readFile(contactsPath);
+        const contacts = JSON.parse(data);
+        const neededContact = contacts.find(contact => contact.id === id);
+        if (!neededContact) return null;
+        neededContact.name = name;
+        neededContact.email = email;
+        neededContact.phone = phone;
+        await fs.promises.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+        return (neededContact)
+    } catch (error) {
+        return null;
+    }
+}
+
+export { listContacts, getContactById, removeContact, addContact, updContact };
