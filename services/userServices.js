093d237d83b629323ac499a01d3059728a04f3b6
Esnobart <offrichterrr@gmail.com>
2024-04-21 14:40:23 +0100

    error run 2

--- services/userServices.js
+++ services/userServices.js
@@ -2,8 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { User } from "../models/userModel.js";
 
 async function createUser(password, email, subscription) {
-    const newUser = await User.create({ id: uuidv4(), email, subscription });
-    newUser.password = undefined;
+    const newUser = await User.create({ id: uuidv4(), password, email, subscription });
     return (newUser);
 };
 
