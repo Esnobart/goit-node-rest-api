654ac3a8227d9a54761e79351731a4de9146bd09
Esnobart <offrichterrr@gmail.com>
2024-04-21 11:38:48 +0100

    updated

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -5,7 +5,7 @@ export const getAllContacts = async (req, res, next) => {
     try {
         const getContacts = await listContacts();
         res.send(getContacts);
-    } catch (err) {next (HttpError(400))}
+    } catch (err) {next (err)}
 };
 
 export const getOneContact = async (req, res) => {
@@ -13,7 +13,7 @@ export const getOneContact = async (req, res) => {
         const getContact = await getContactById(req.params.id);
         if (!getContact) {next (HttpError(404));}
         res.status(200).send(getContact)
-    } catch (err) {next (HttpError(400))}
+    } catch (err) {next (err)}
 };
 
 export const deleteContact = async (req, res) => {
@@ -21,7 +21,7 @@ export const deleteContact = async (req, res) => {
         const delContact = await removeContact(req.params.id);
         if (!delContact) {next (HttpError(404));}
         res.status(200).send(delContact)
-    } catch (err) {next (HttpError(400))}
+    } catch (err) {next (err)}
 };
 
 export const createContact = async (req, res) => {
@@ -30,7 +30,7 @@ export const createContact = async (req, res) => {
         const newContact = await addContact(name, email, phone, favorite);
         if (!newContact) {next (HttpError(404));};
         res.status(201).send(newContact)
-    } catch (err) {next (HttpError(400))}
+    } catch (err) {next (err)}
 };
 
 export const updateContact = async (req, res) => {
@@ -38,12 +38,12 @@ export const updateContact = async (req, res) => {
         const { name, email, phone } = req.body;
         const upContact = await updContact(req.params.id, name, email, phone);
         res.status(200).send(upContact)
-    } catch (err) {next (HttpError(400))}
+    } catch (err) {next (err)}
 };
 
 export const updateFavorite = async (req, res) => {
     try {
         const updatedFavorite = await updFavorite(req.params.id, req.body.favorite);
         res.status(200).send(updatedFavorite)
-    } catch (err) {next (HttpError(400))}
+    } catch (err) {next (err)}
 }
